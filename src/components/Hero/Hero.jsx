f8b3fc4e2dcb2e2f7650bbdfe1c39b6ffa44d713
Jeffrey Nibbs <djcountry100@gmail.com>
2023-04-02 05:06:23 -0700

    test

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
     <div className='header-container'>
       <div className='hero'>
         <div className='cta'>
-          <h1 className='cta-title'>Fuck wit us</h1>
+          <h1 className='cta-title'>Who's your daddy</h1>
           <h3 className='cta-text'>I know you want to</h3>
           <button className='cta-btn'>Hit me up</button>
         </div>
